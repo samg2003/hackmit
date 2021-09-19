7c81271b70b25250f3744606d6643c8b6e504baa
Sambhav Gupta <sambhav2003gupta@gmail.com>
2021-09-19 03:41:55 -0500

    added figure compatibitlity

--- application.py
+++ application.py
@@ -3,7 +3,9 @@ import os
 from flask import Flask, render_template, session, request, redirect, url_for, jsonify , make_response
 from flask_session import Session
 import time
-app = Flask(__name__)
+import requests
+from utilities import *
+app = Flask(__name__, static_folder = "images")
 
 
 
@@ -18,6 +20,14 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        coordinate = request.form.get("coordinates")
+        if coordinate == None:
+            return render_template("index.html", message = "Something went wrong")
+        coordinate = coordinate.split(",")
+        coordinate = [float(coordinate[0].strip()), float(coordinate[1].strip())]
+        year, name = coordinates(coordinate[0], coordinate[1])
+        return render_template("index.html", message = year, path = name)
     return render_template("index.html")
 
 '''
--- utilities.py
+++ utilities.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.vector_ar.var_model import VAR
+from random import random
+import requests
+import time
+import os
+
 a = []
 with open("data.txt", 'r') as file:
     while file.readline():
@@ -8,4 +15,71 @@ for line in range(len(a)):
     for number in range(len(a[line])):
         a[line][number] = float(a[line][number])
 
-print(a[1], a[102])
+column_names = [
+    'altimeter_type',
+    'cycle',
+    'year',
+    'observation_count',
+    'observation_count_weighted',
+    'gmsl_variation',
+    'gmsl_variation_std',
+    'gmsl_variation_smooth',
+    'gmsl_variation_with_gia',
+    'gmsl_variation_with_gia_std',
+    'gmsl_variation_with_gia_smooth',
+    'gmsl_variation_with_gia_smooth_and_signals_removed',
+]
+df = pd.DataFrame(a, columns = column_names)
+
+df["gmsl_variation_with_gia_smooth_and_signals_removed"] = df["gmsl_variation_with_gia_smooth_and_signals_removed"] + abs(min(df["gmsl_variation_with_gia_smooth_and_signals_removed"]))
+
+
+
+
+
+
+
+def elevation(lat, long):
+    r = requests.get('https://api.open-elevation.com/api/v1/lookup?locations=' + str(lat) + "," + str(long))
+    elevation = r.json()["results"][0]["elevation"]
+    print(elevation)
+def empty_directory(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                os.remove(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+def coordinates(lat, long):
+    r = requests.get('https://api.open-elevation.com/api/v1/lookup?locations=' + str(lat) + "," + str(long))
+    elevation = r.json()["results"][0]["elevation"]
+    model = VAR(df[['year','gmsl_variation_with_gia_smooth_and_signals_removed']])
+    model_fit = model.fit()
+    yhat = model_fit.forecast(model_fit.y, steps=1)
+    data = df[['year','gmsl_variation_with_gia_smooth_and_signals_removed']]
+    predicted = data.append(pd.DataFrame(yhat, columns=data.columns), ignore_index=True)
+    if elevation > 10:
+        return "There is plenty of elevation at this place."
+    while yhat[0][1] < elevation * 100:
+        model = VAR(predicted[['year','gmsl_variation_with_gia_smooth_and_signals_removed']])
+        model_fit = model.fit()
+        yhat = model_fit.forecast(model_fit.y, steps=1)
+        num = 2.3
+        if random() > 0.5: num = -2
+        yhat[0][1] += num * random()
+        data = predicted[['year','gmsl_variation_with_gia_smooth_and_signals_removed']]
+        predicted = data.append(pd.DataFrame(yhat, columns=data.columns), ignore_index=True)
+    predicted["gmsl_variation_with_gia_smooth_and_signals_removed"] = predicted["gmsl_variation_with_gia_smooth_and_signals_removed"] + abs(min(predicted["gmsl_variation_with_gia_smooth_and_signals_removed"]))
+    fig = predicted.plot(x='year', y='gmsl_variation_with_gia_smooth_and_signals_removed',figsize=(14, 7), zorder=2)
+    fig.set_ylabel("elevation of sea in cm")
+    fig.legend("")
+    name = "images/" + str(random()) + ".png"
+    empty_directory("images")
+    fig.figure.savefig(name)
+    plt.cla()
+    plt.close()
+    time.sleep(1)
+    return (int(str(predicted["year"][len(predicted) - 1]).split(".")[0])), name
